fa5481afcde217dd1c0a7e66137caa371d734275
teotia-codes <priyanshu.teotia50@gmail.com>
2024-03-14 01:14:22 +0530

    onboardingScreen done

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -11,12 +11,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.example.newsapp.presentation.onboarding.OnBoardingScreen
 import com.example.newsapp.ui.theme.NewsAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window,false)
         installSplashScreen()
         setContent {
             NewsAppTheme(dynamicColor = false) {
--- app/src/main/java/com/example/newsapp/presentation/onboarding/onBoardingScreen.kt
+++ app/src/main/java/com/example/newsapp/presentation/onboarding/onBoardingScreen.kt
@@ -2,11 +2,17 @@ package com.example.newsapp.presentation.onboarding
 
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
@@ -15,10 +21,18 @@ import androidx.compose.runtime.Composable
 
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.newsapp.presentation.common.NewButton
+import com.example.newsapp.presentation.common.NewsTextButton
 
 import com.example.newsapp.presentation.onboarding.components.OnBoardingPage
+import com.example.newsapp.presentation.onboarding.components.PageIndicator
+import kotlinx.coroutines.launch
+
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -40,9 +54,53 @@ fun OnBoardingScreen() {
         HorizontalPager(state = pagerState) { index ->
             OnBoardingPage(page = pages[index])
         }
-Spacer(modifier = Modifier.weight(1f))
+        Spacer(modifier = Modifier.weight(1f))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 30.dp)
+                .navigationBarsPadding(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            PageIndicator(
+                modifier = Modifier.width(52.dp),
+                pageSize = pages.size,
+                selectedPage = pagerState.currentPage
+            )
 
-  }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                val scope = rememberCoroutineScope()
+                //Hide the button when the first element of the list is empty
+                if (buttonsState.value[0].isNotEmpty()) {
+                    NewsTextButton(
+                        text = buttonsState.value[0],
+                        onClick = {
+                            scope.launch {
+                                pagerState.animateScrollToPage(
+                                    page = pagerState.currentPage - 1
+                                )
+                            }
+
+                        }
+                    )
+                }
+                NewButton(
+                    text = buttonsState.value[1],
+                    onClick = {
+                        scope.launch {
+                            if (pagerState.currentPage == 3){
+                                //Navigate to the main screen and save a value in datastore preferences
+                            }else{
+                                pagerState.animateScrollToPage(
+                                    page = pagerState.currentPage + 1
+                                )
+                            }
+                        }
+                    }
+                )
+            }
+        }
+        Spacer(modifier = Modifier.weight(0.5f))
+    }
 }
-
-
